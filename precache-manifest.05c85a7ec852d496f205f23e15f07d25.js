07ae7f7138c8a21df77b98f926a17f16d8bc8efd
Piotr Kraszewski <kraszewskipiotr1@gmail.com>
2020-03-15 14:04:17 +0100

    Updates

---
+++ precache-manifest.05c85a7ec852d496f205f23e15f07d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2fbea09ece940c4805eb9612f23aae6",
+    "url": "/React-TodoForm/index.html"
+  },
+  {
+    "revision": "fb02f55f127250e1bcfd",
+    "url": "/React-TodoForm/static/css/main.beea655f.chunk.css"
+  },
+  {
+    "revision": "99200814988080811232",
+    "url": "/React-TodoForm/static/js/2.681e5d8e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-TodoForm/static/js/2.681e5d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb02f55f127250e1bcfd",
+    "url": "/React-TodoForm/static/js/main.1fa1a71a.chunk.js"
+  },
+  {
+    "revision": "8b1466e0a2d3211e9858",
+    "url": "/React-TodoForm/static/js/runtime-main.a2a27f83.js"
+  }
+]);
